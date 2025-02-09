c478ea507134d133805e4be76557863e35a8af51
Sandeep <sandeepdewangan7@gmail.com>
2025-02-09 22:12:34 +0530

    Box Model

--- README.md
+++ README.md
@@ -6,5 +6,9 @@
 2. Background Color: `bg-yellow-200`
 3. Font size: `text-3xl`
 4. Font weight: `font-semibold`
-5. Letter Spacing: `tracking-widest` | `tracking-[1em]`
-6.
+5. Letter spacing: `tracking-widest` | `tracking-[1em]`
+6. Alignment of text: `text-center` | `text-justify` | `text-start`
+7. Padding and Margin: `px-2` | `mx-2`
+8. Border: `border-b-2 border-orange-300`
+9. Spacing between two elements: `space-x-4`
+10.
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 function App() {
   return (
     <>
-      <h1 className="bg-yellow-200 tracking-[1em]">Header</h1>
+      <div className="bg-yellow-200 py-4 px-2 text-center border-b-2 border-orange-300">
+        <span className="tracking-[1em]">Header</span>
+      </div>
       <h1 className="text-3xl font-semibold text-yellow-500">Hello</h1>
+      <div className="space-x-4">
+        <span>Hello</span>
+        <span>Bye</span>
+      </div>
     </>
   );
 }
