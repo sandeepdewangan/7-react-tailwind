21da490c9a4d9d15141447f2e27c5667ce87f79c
Sandeep <sandeepdewangan7@gmail.com>
2025-02-09 22:53:45 +0530

    Flex

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # React
 
-### Colors
+### Important Properties
 
 1. Text Color: `text-red-500`
 2. Background Color: `bg-yellow-200`
 3. Font size: `text-3xl`
 4. Font weight: `font-semibold`
 5. Letter spacing: `tracking-widest` | `tracking-[1em]`
-6. Alignment of text: `text-center` | `text-justify` | `text-start`
+6. Alignment of text (horizontally): `text-center` | `text-justify` | `text-start`
 7. Padding and Margin: `px-2` | `mx-2`
 8. Border: `border-b-2 border-orange-300`
 9. Spacing between two elements: `space-x-4`
 10. Box model setting width and height: `h-80` | `w-71`
-11.
+
+### Flex
+
+`flex` -> Arrange items in one row.
+`justify-between` | `justify-around` -> Pull items into specified way horizontally.
+`items-end` | `items-center` -> Vertical alignment of items.
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 function App() {
   return (
-    <>
-      <div className="bg-yellow-200 py-4 px-2 text-center border-b-2 border-orange-300">
+    <div>
+      <div className="text-2xl bg-yellow-200 py-4 px-2 border-b-2 border-orange-300">
         <span className="tracking-[1em]">Header</span>
       </div>
-      <h1 className="text-3xl font-semibold text-yellow-500">Hello</h1>
-      <div className="space-x-4">
-        <span>Hello</span>
-        <span>Bye</span>
+      <div className="px-4">
+        <h1 className="text-3xl font-semibold text-yellow-500 text-center">
+          Hello
+        </h1>
+        <div className="flex justify-between ">
+          <p className="space-x-2">
+            <span>Hello</span>
+            <span>Bye</span>
+          </p>
+          <span>ADD TO CART</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
