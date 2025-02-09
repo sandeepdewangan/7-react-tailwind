21a07abfcd9195976ce937bfbb7bffa1ca365903
Sandeep <sandeepdewangan7@gmail.com>
2025-02-09 22:00:36 +0530

    Text Styling

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 1. Text Color: `text-red-500`
 2. Background Color: `bg-yellow-200`
-3.
+3. Font size: `text-3xl`
+4. Font weight: `font-semibold`
+5. Letter Spacing: `tracking-widest` | `tracking-[1em]`
+6.
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <h1 className="bg-yellow-200">Header</h1>
-      <h1 className="text-3xl text-yellow-500">Hello</h1>
+      <h1 className="bg-yellow-200 tracking-[1em]">Header</h1>
+      <h1 className="text-3xl font-semibold text-yellow-500">Hello</h1>
     </>
   );
 }
